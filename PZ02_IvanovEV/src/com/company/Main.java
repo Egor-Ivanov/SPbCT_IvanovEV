4a97a5a5c6b098089c5fb6f37bda38b86b26cd37
Egor-Ivanov <49913628+Egor-Ivanov@users.noreply.github.com>
2021-01-19 11:19:50 +0300

    Add files via upload

---
+++ PZ02_IvanovEV/src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* char symb1=1067;
+        char symb2 ='Ы';
+
+        System.out.println("symb1 contains "+ symb1);
+        System.out.println("symb2 contains "+ symb2);
+///////////////////////////////////////////////////
+        char ch = 'J';
+        int intCh = (int) ch;
+
+        System.out.println("J corresponds with "+ intCh);
+////////////////////////////////////////////////////////
+        String s1 = "I have ";
+        String s2 = " apples ";
+        int num = 3;
+        String s = s1 + num + s2;
+
+        System.out.println(s);
+/////////////////////////////////////////////////////////
+        int i = 35;
+        String str = Integer.toString(i);
+        System.out.println(str);
+//////////////////////////////////////////////////////////
+        char ch = 'S';
+        String charToString = Character.toString(ch);
+        System.out.println(charToString);
+/////////////////////////////////////////////////////////
+        int i = 2015;
+        long l = (long) (i);
+        System.out.println(l);*/
+        //////////////////////////////////////////////////
+        byte x=5;
+        byte y1=x++;
+        byte y2=x--;
+        byte y3=++x;
+        byte y4=--x;
+        System.out.println(y1);
+        System.out.println(y2);
+        System.out.println(y3);
+        System.out.println(y4);
+
+
+    }
+}
